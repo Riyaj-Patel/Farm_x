23f89393a36fa1c123f58cddef31a399596cd93e
Riyaj Patel <riyajpatel041@gmail.com>
2020-09-29 05:24:12 -0700

    finalize temp and moisture

--- src/Getambee.js
+++ src/Getambee.js
@@ -1,61 +1,46 @@
-import React, { Component } from 'react';
+import React, {useState,useEffect } from 'react';
 import {usePosition} from './usePosition';
-import axios from 'axios';
 
-
-function Getambee  ({watch, settings}) {
-      let  soil_temperature=null;
-       let soil_moisture=null;
-    
+function Getambee  ({watch, settings}) {   
     const limit="1";
     const {
       latitude,
       longitude,
-      timestamp,
-      accuracy,
       error,
     } = usePosition(watch, settings);
-  
+    const [soil_temperature, settemp] = useState(0);
+    const [soil_moisture, setmoist] = useState(0); 
     const loader = !latitude && !error ? (
       <>
         <div>Trying to fetch location...</div>
         <br/>
       </>
     ) : null;
-
-  console.log(latitude);
-  
-        let URL =`https://api.ambeedata.com/soil/latest/by-lat-lng?lat=${latitude}&lng=${longitude}&limit=${limit}`
-        fetch(URL,{
+    
+    useEffect(() => {  
+      if (!latitude) return;   
+        fetch(`https://api.ambeedata.com/soil/latest/by-lat-lng?lat=${latitude}&lng=${longitude}&limit=${limit}`,
+        {
             method:'GET',
             headers:{
                 'x-api-key':'tLWZol1niJ1yw0nVa312H9xOVxr0UpcS8OK36sBr',
                 'accept':'application/json'
-            },
-            
-            
-            
+            },    
         }).then((result)=>{
             result.json().then((res)=>{
-                console.log("a" ,res)
-                getresult(res)
+                console.log("a" ,res);
+                settemp(res.data[0].soil_temperature);
+                setmoist(res.data[0].soil_moisture);
             })
         })
-       function getresult(res){                
-                soil_moisture=res.data[0].soil_moisture;
-                soil_temperature=res.data[0].soil_temperature;
-                 
-                 console.log("es",res.data[0]);
-                }
-    
+    },[latitude,longitude]);
   
   return (
     <div>
       latitude: {latitude}<br/>
       longitude: {longitude}<br/>
-      error: {error}
-      accuracy: {accuracy && `${accuracy}m`}<br/>
       temprature: {soil_temperature}<br/>
+      moisture: {soil_moisture}<br/>
     </div>
   );
 };
